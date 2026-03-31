77031003c14d0376228713667ade7dd75d8c227f
Aritra Chowdhury <ac.is.working245@gmail.com>
2026-03-31 20:03:41 +0530

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,21 @@
+import platform
+import os
+from datetime import datetime
+import getpass
+
+student_name = "Abhradwip Lala"
+software = "Python"
+
+print("========================================")
+print(f"   Open Source Audit — {student_name}")
+print("========================================")
+
+print("Software :", software)
+print("Distro   :", platform.platform())
+print("Kernel   :", platform.release())
+print("User     :", getpass.getuser())
+print("Home Dir :", os.path.expanduser("~"))
+print("Date     :", datetime.now())
+
+print("License  : Python is licensed under PSF License")
+print("========================================")
